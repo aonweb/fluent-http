171ec06394f77bb64b3a50aca7a97b232c0293e9
Adam Anderson <adanderson@amctheatres.com>
2015-11-23 17:03:44 -0600

    additional unit tests

---
+++ AonWeb.FluentHttp.Tests/HelpersTests.cs
@@ -0,0 +1,67 @@
+﻿using AonWeb.FluentHttp.Exceptions;
+using AonWeb.FluentHttp.Helpers;
+using AonWeb.FluentHttp.Tests.Helpers;
+using Shouldly;
+using Xunit;
+
+namespace AonWeb.FluentHttp.Tests
+{
+    public class TypeHelpersTests
+    {
+        [Fact]
+        public void ValidateType_WhenTypeIsTheSame_ExpectNoExceptionReturnTrue()
+        {
+            var result = TestResult.Default1();
+
+            var actual = TypeHelpers.ValidateType(result, typeof (TestResult));
+
+            actual.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ValidateType_WhenRequestedTypeIsSmaller_ExpectNoExceptionReturnTrue()
+        {
+            var result = new AlternateTestResult();
+
+            var actual = TypeHelpers.ValidateType(result, typeof(TestResult));
+
+            actual.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ValidateType_WhenTypeIsLarger_ExpectException()
+        {
+            var result = TestResult.Default1();
+
+            Should.Throw<TypeMismatchException>(() => TypeHelpers.ValidateType(result, typeof(AlternateTestResult)));
+        }
+
+        [Fact]
+        public void ValidateType_WhenTypeAreNotSame_ExpectException()
+        {
+            var result = TestResult.Default1();
+
+            Should.Throw<TypeMismatchException>(() => TypeHelpers.ValidateType(result, typeof(TestResource)));
+        }
+
+        [Fact]
+        public void ValidateType_WhenTypeIsLargerAndSuppressTypeMismatch_ExpectNoExceptionReturnFalse()
+        {
+            var result = TestResult.Default1();
+
+            var actual = TypeHelpers.ValidateType(result, typeof(AlternateTestResult), true);
+
+            actual.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void ValidateType_WhenTypeAreNotSameAndSuppressTypeMismatch_ExpectNoExceptionReturnFalse()
+        {
+            var result = TestResult.Default1();
+
+            var actual = TypeHelpers.ValidateType(result, typeof(TestResource), true);
+
+            actual.ShouldBeFalse();
+        }
+    }
+}
