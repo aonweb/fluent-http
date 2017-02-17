72533d5061036598bf9026dc3c171b9653d25759
Adam Anderson <adanderson@amctheatres.com>
2017-02-17 17:05:21 -0600

    revved version

--- AonWeb.FluentHttp.Serialization/Properties/AssemblyInfo.cs
+++ AonWeb.FluentHttp.Serialization/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.5")]
-[assembly: AssemblyFileVersion("3.1.5")]
-[assembly: AssemblyInformationalVersion("3.1.5")]
+[assembly: AssemblyVersion("3.1.6")]
+[assembly: AssemblyFileVersion("3.1.6")]
+[assembly: AssemblyInformationalVersion("3.1.6")]
