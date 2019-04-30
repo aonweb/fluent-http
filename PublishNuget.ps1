e45f9cfbf9434c83bda48334c318b1a20a7ba04c
Adam Anderson <adanderson@amctheatres.com>
2019-04-29 20:27:49 -0500

    cleanup nuget publish

---
+++ PublishNuget.ps1
@@ -0,0 +1,61 @@
+﻿param (
+    [string]$source = "http://svd0nugetd01/nuget/",
+	[string]$nuget = "tools\nuget.exe"
+ )
+ 
+ 
+function DisplayArgs()
+{
+	"Options"
+    "    => source: $source"
+    "    => nuget: $nuget"
+
+    "examples"
+    "  publish-nuget.ps1"
+    "  publish-nuget.ps1 -source http://mynuget/nuget/"
+    "  publish-nuget.ps1 -source http://mynuget/nuget/ -nuget c:\nuget\nuget.exe"
+    ""
+
+	if (-Not $nuget -eq "")
+    {
+        $global:nugetExe = $nuget
+    }
+    else
+    {
+        # Assumption, nuget.exe is in "..\tools\nuget.exe".
+        $global:nugetExe = "tools\nuget.exe" 
+    }
+
+    $global:nugetExe
+
+    if (!(Test-Path $global:nugetExe -PathType leaf))
+    {
+        ""
+        "Nuget exe was not found. "
+        ""
+        throw;
+    }
+}
+
+function Push()
+{
+	$directories = Get-ChildItem -Path . -File -Filter "*.nupkg" -Recurse | ? { $_.FullName -match '.*\\Release\\*.*' } | Select-Object -Property Directory -Unique
+
+    foreach($directory in $directories)
+    {
+		$latest = Get-ChildItem -Path $directory.Directory -File -Filter "*.nupkg" | Sort-Object LastAccessTime -Descending | Select-Object -First 1
+		
+        &$nugetExe push ($latest.FullName) -Source $source
+
+        ""
+    }
+}
+
+$ErrorActionPreference = "Stop"
+$global:nugetExe = ""
+
+cls
+
+DisplayArgs
+
+Push
