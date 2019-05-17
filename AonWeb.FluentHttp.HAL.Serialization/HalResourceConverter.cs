2442fb63424a9084767c0e6caaca6c80395999e6
Adam Anderson <adanderson@amctheatres.com>
2019-05-17 12:33:24 -0500

    moving hal converter to serialization library
    adjusting version

--- AonWeb.FluentHttp.HAL.Serialization/HalResourceConverter.cs
+++ AonWeb.FluentHttp.HAL.Serialization/HalResourceConverter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using AonWeb.FluentHttp.Helpers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
